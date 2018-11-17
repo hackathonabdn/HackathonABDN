343898ae958a4b22f69097ec717f81ba57252c0d
Sadiq <s.sani@rgu.ac.uk>
2018-11-17 12:52:56 +0000

    added code for window matching

--- load-data.py
+++ load-data.py
@@ -1,7 +1,28 @@
 from welly import Well
 import matplotlib.pyplot as plt
-import geocop_utils
+import geocomp_utils as gu
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
 
 p = Well.from_las('las-files/6307_d.las')
 
 gr = p.data['GR']
+
+gr[np.isnan(gr)] = 0
+
+windows = gu.split_windows(gr, 10)
+
+X = gu.extract_time_features(windows, None)
+pattern = X[0, :]
+pattern = pattern.reshape(-1, 20)
+X_train = X[1: , :]
+
+nn = NearestNeighbors(metric='cosine')
+nn.fit(X_train)
+sims, indices = nn.kneighbors(pattern)
+
+for sim, indx in zip(sims[0], indices[0]):
+    print(str(indx)+' : '+str(sim))
+
+
+
