53a30cd2f5965079e0f2e38a8c748ff47c61e959
khaled <khaled.ben.rhouma1999@gmail.com>
2018-11-18 11:14:58 +0000

    added 2 routes

--- frontend/app.py
+++ frontend/app.py
@@ -1,11 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 from welly import Well
+import numpy as np
 import matplotlib.pyplot as plt
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!' 
+def home():
+    return 'Geological Formation' 
 
 @app.route('/about')
 def about():
@@ -15,9 +16,9 @@ def about():
 def gasoil():
     return render_template('gasoil.html')
 
-@app.route('/GeologicalFormation')
-def GFormation():
-    return render_template('geological-formation.html')
+@app.route('/Gformation')
+def Gformation():
+    return render_template('Gformation.html')
     
 @app.route('/plot')
 def plot():
@@ -26,6 +27,24 @@ def plot():
     plt.savefig('static/images.png')
     return render_template('base.html',name = 'plot', url='static/images.png')
     
+@app.route('/getplotdata/<wellname>')
+def getplotdata(wellname):
+    p = Well.from_las('static/las-files/'+wellname+'.las')
+    gr = p.data['GR']
+    gr [np.isnan(gr)] = 0
+    payload = {'curve': list(gr),
+    'depth': list(gr.basis)}
+    return jsonify(payload)  
+
+@app.route('/retrieve/<wellname>/<fr>/<to>')
+def retrieve(wellname,fr,to):
+    p = Well.from_las('static/las-files/'+wellname+'.las')
+    gr = p.data['GR']
+    gr [np.isnan(gr)] = 0
+    payload = {'curve': list(gr),
+    'depth': list(gr.basis)}
+    return jsonify(payload)
+        
 if __name__ == '__main__':
     app.run()
 
